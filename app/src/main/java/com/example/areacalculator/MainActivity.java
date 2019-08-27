d5c4d324a89529d99abcf52692f60f904db12c20
Harshitha KV <kv.harshitha92@gmail.com>
2019-08-27 18:18:32 -0400

    final commit

--- app/src/main/java/com/example/areacalculator/MainActivity.java
+++ app/src/main/java/com/example/areacalculator/MainActivity.java
@@ -1,4 +1,11 @@
 package com.example.areacalculator;
+/*
+Assignment Number :In Class Assignment 2
+File Name : MainActivity.java
+Full Name : Team: 15
+            Sahil Sood
+            Harshitha Keshavaraju Vijayalakshmi
+ */
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,16 +13,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageViewTriangle, imageViewCircle, imageViewSquare;
     private TextView textViewShapeSelected, textView2, length2, length1, textViewResult;
     double len1, len2;
-    private Button clearButton,calculateButton;
+    private Button clearButton, calculateButton;
     String selectedShape;
 
 
@@ -37,53 +44,56 @@ public class MainActivity extends AppCompatActivity {
         imageViewCircle = (ImageView) findViewById(R.id.imageViewCircle);
         imageViewSquare = (ImageView) findViewById(R.id.imageViewSquare);
 
+
         clearButton();
-        len1 =0.0;
-        len2 =0.0;
         calculateButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 if (length1.getText().toString().equals("")) {
-                    length1.setError("hey! i need a value!");
-                }
-                else{
+                    length1.setError("hey! I need a value!");
+                    len1 = 0;
+                } else {
                     len1 = Double.parseDouble(length1.getText().toString());
+                    length1.setError(null);
                 }
                 if (length2.getText().toString().equals("")) {
-                    length2.setError("hey! i need a value!");
-                }
-                else{
+                    length2.setError("hey! I need a value!");
+                    len2 = 0;
+                } else {
                     len2 = Double.parseDouble(length2.getText().toString());
+                    length1.setError(null);
                 }
-                double result= 0;
+                double result = 0;
                 switch (selectedShape) {
                     case "Triangle":
                         Log.i("shape selected", selectedShape);
-                        result = .5 * len1 * len2;
+                        result = 0.5 * len1 * len2;
+                        result = Math.round(result * 100.0) / 100.0;
                         textViewResult.setText(Double.toString(result));
                         Log.d("result for triangle", Double.toString(result));
                         break;
                     case "Circle":
                         Log.i("shape selected", selectedShape);
-                        result = Math.round(3.14 * Math.pow(len1,2));
+                        result = 3.1416*len1*len1;
+                        result = Math.round(result * 100.0) / 100.0;
                         textViewResult.setText(Double.toString(result));
                         Log.d("result for circle", Double.toString(result));
                         break;
                     case "Square":
                         Log.i("shape selected", selectedShape);
                         result = len1 * len1;
-
+                        result = Math.round(result * 100.0) / 100.0;
                         textViewResult.setText(Double.toString(result));
-
                         Log.d("result for Square", Double.toString(result));
                         break;
                     default:
-                        Toast.makeText(MainActivity.this, "No shape selected!", Toast.LENGTH_SHORT).show();
+                        textViewShapeSelected.setError("No shape selected!");
                 }
 
             }
         });
+
         clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -95,12 +105,13 @@ public class MainActivity extends AppCompatActivity {
         imageViewTriangle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textViewShapeSelected.setError(null);
                 textViewResult.setText("");
                 textView2.setVisibility(View.VISIBLE);
                 length2.setVisibility(View.VISIBLE);
                 textViewShapeSelected.setText("Triangle");
                 selectedShape = textViewShapeSelected.getText().toString();
-              //  Log.d("selected shape", selectedShape);
+                //  Log.d("selected shape", selectedShape);
 
 
             }
@@ -108,29 +119,29 @@ public class MainActivity extends AppCompatActivity {
         imageViewCircle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textViewShapeSelected.setError(null);
                 textViewResult.setText("");
                 textViewShapeSelected.setText("Circle");
                 textView2.setVisibility(View.GONE);
                 length2.setVisibility(View.GONE);
                 selectedShape = textViewShapeSelected.getText().toString();
-             //   Log.d("selected shape", selectedShape);
+                //   Log.d("selected shape", selectedShape);
             }
         });
         imageViewSquare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textViewShapeSelected.setError(null);
                 textViewResult.setText("");
                 textViewShapeSelected.setText("Square");
                 textView2.setVisibility(View.GONE);
                 length2.setVisibility(View.GONE);
                 selectedShape = textViewShapeSelected.getText().toString();
-             //   Log.d("selected shape", selectedShape);
 
             }
         });
 
     }
-
 
     public void clearButton() {
         length1.setText("");
@@ -139,9 +150,11 @@ public class MainActivity extends AppCompatActivity {
         length2.setText("");
         textView2.setVisibility(View.VISIBLE);
         length2.setVisibility(View.VISIBLE);
+        textViewShapeSelected.setError(null);
         textViewShapeSelected.setText("Select a shape");
         textViewResult.setText("");
-        selectedShape ="";
+        selectedShape = "";
+
     }
 
 }
